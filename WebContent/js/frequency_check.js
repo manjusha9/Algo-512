45f6cfd3d2e8c839828f12b14eed8544aff3711f
Harshil Patel <patel_harshil_h@yahoo.com>
2016-05-02 20:44:26 -0400

    Final Uploading

---
+++ WebContent/js/frequency_check.js
@@ -0,0 +1,152 @@
+	var frequentElements= new Map();
+function frequencyCheck(stream,freq)
+{	
+	stream=stream.replace(/[^A-Z0-9]/ig, " ");
+	stream=stream.replace(/\s\s+/g, ' ');
+	stream=stream.trim();
+	
+	var elements=stream.split(" ");
+	
+	
+	console.log(elements);
+	
+	//k=N/K
+	var k =parseInt(elements.length/freq);
+	
+	
+	var list = document.getElementsByClassName("stream-string");
+	  for (var i = 0; i < list.length; i++) {
+	   list[i].setAttribute("id", "strings" + i);
+	  }
+	  
+	var n=0;
+	
+	console.log(elements.length);
+	
+	var i=0;
+	
+	//First pass
+	var intervalId=setInterval(function(){
+		document.getElementById("mybutton").style.display = 'none';
+		
+		document.getElementById("new-button").innerHTML= "<form action='operation.jsp'><button type='submit' class='button-block' id='back-button' disabled>Back</button></form>"
+
+		n=n+1;
+		if(i>0 && i<=elements.length)
+			{
+			var streamDivOld=document.getElementById(("strings"+(i-1).toString()).toString());
+			streamDivOld.style.backgroundColor="#fdfdfd";
+			}
+		
+		if(i<elements.length)
+			{
+			var streamDiv=document.getElementById(("strings"+i.toString()).toString());
+			streamDiv.style.backgroundColor="#0AF71E";
+			
+			algorithm(elements[i],k);
+			
+			var list = document.getElementsByClassName("loader-block");
+			  for (var m = 0; m < list.length; m++) {
+			   list[m].innerHTML="";
+			  }
+			
+			var list1 = document.getElementsByClassName("loader-block1");
+			  for (var m = 0; m < list1.length; m++) {
+			   list1[m].innerHTML="";
+			  }
+			
+			var p=0;
+			frequentElements.forEach(function(value, key) {
+			console.log("p ="+p);	  
+			var assoDiv=document.getElementById("array"+p.toString());	
+			assoDiv.innerHTML=key.toString();
+			console.log("p ="+p);	
+			var counterDiv=document.getElementById("counter"+p.toString());	
+			counterDiv.innerHTML=value.toString();
+			p++;
+			}, frequentElements);
+			
+			}
+		
+		if(i>elements.length)
+			{
+			
+			clearInterval(intervalId);
+			document.getElementById("mybutton").disabled = false;
+			document.getElementById('state').style.display = "none";
+			frequentElements.forEach(function(value, key){
+				
+			},frequentElements);
+		
+		var count=0;
+		
+		var finalDisp=document.getElementById("final-display");
+		
+		finalDisp.innerHTML=finalDisp.innerHTML+ "<div class='asso' style='color:black;'>Second Pass</div>";
+		
+		//Second pass
+		frequentElements.forEach(function(value, key) {
+			count=0;
+			finalDisp.innerHTML=finalDisp.innerHTML + "<br/><span><div class='loader-block' style='text-align:middle;'>"+key+"</div>";
+			for(var j = 0, len3 = elements.length; j < len3; j++)
+			{
+				
+				if(elements[j]==key)
+				{
+					count++;
+				}
+			}
+			
+			finalDisp.innerHTML=finalDisp.innerHTML + "<div class='loader-block1' style='text-align:middle;'>"+count.toString()+"</div><br/></span>";
+			
+			if(count<freq)
+			{	
+				frequentElements.delete(key);
+			}
+		},frequentElements);
+
+		console.log(frequentElements.size);
+		
+		finalDisp=document.getElementById("final-output");
+		
+		finalDisp.innerHTML="<div class='f-out' id='final-data'>The words with frequency greater than or equal to "+freq+" are </div>";
+		
+		finalDisp=document.getElementById("final-data");
+		frequentElements.forEach(function(value, key){
+		console.log(value+" "+key);
+		finalDisp.innerHTML=finalDisp.innerHTML+"<span style='font-weight:bold;'>"+key+","+"</span>";
+		},frequentElements);
+		frequentElements.clear();
+		document.getElementById("back-button").disabled=false;
+			}		
+		i++;		
+	}, 1000);	
+}
+
+
+function algorithm(element,k)
+{
+	//ALready there
+	if(frequentElements.has(element))
+	{		
+		frequentElements.set(element,parseInt(frequentElements.get(element))+1);
+	}
+	//First entry and Associative array not full
+	else if(frequentElements.size <k-1)
+	{	
+		frequentElements.set(element,1);		
+	}
+	//Associative array full and new entry
+	else	{
+		
+			frequentElements.forEach(function(value, key) {
+				  frequentElements.set(key,frequentElements.get(key)-1);				
+				
+			if(frequentElements.get(key)==0)
+			{				
+				frequentElements.delete(key);
+			}
+			}, frequentElements);		
+	}
+
+}
---
+++ src/com/models/RegistrationClass.java
@@ -0,0 +1,44 @@
+package com.models;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class RegistrationClass{
+
+	public String storeUserDetails(String userName, String password, String firstName, String lastName,
+			String emailId, String phoneNo) {
+		
+			
+		SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+		Session session=sessionFactory.openSession();
+		session.beginTransaction();
+		
+		
+		UserDetails user=null;
+		
+		user=(UserDetails)session.get(UserDetails.class,userName);
+		
+		if(user==null)
+		{
+			user=new UserDetails();
+			user.setUserName(userName);
+			user.setPassword(password);
+			user.setFirstName(firstName);
+			user.setLastName(lastName);
+			user.setEmailId(emailId);
+			user.setPhoneNo(phoneNo);
+		
+			session.save(user);
+			session.getTransaction().commit();
+			session.close();
+			return "success";
+		}
+		else
+		{
+			return "exists";
+		}
+	
+	}
+}
